af7f2bc3d0ab83a4dede5ad419907a6a91b8006e
Linus Gustavsson <155681471+LGustavsson@users.noreply.github.com>
2026-05-15 22:58:25 +0200

    V2
    V2

--- DigitaliseraBetygGUI.py
+++ DigitaliseraBetygGUI.py
@@ -1,111 +1,96 @@
-import FreeSimpleGUI as sg
-import os
+# Standardmoduler.
 import sys
 
+# Tredjepartsmoduler.
+import FreeSimpleGUI as sg
+
+# Egna, lokala moduler.
+from Data import Data
 from DigitaliseraBetygSettings import DigitaliseraBetygSettings
+
 
 class DigitaliseraBetygGUI:
     def __init__(self):
-        self.theme = "Green Mono"   # https://user-images.githubusercontent.com/46163555/202543904-e7fefe99-2c02-4e58-9a97-ef0724aa4d8a.png
-        self.icon = f"{os.path.dirname(__file__)}/icon.ico"
-        self.font = ("arial", "12" ,"normal")
-        self.font_output = ("arial", "10" ,"normal")    # Output-texten blir lite mindre. 
-        self.error_color = "#f0b6a4"
-        self.error_list_end = [
-            "",
-            "Var god fyll i nödvändig information via programmet 'DigitaliseraBetygSettings' som finns i programmappen", 
-            "Starta programmet DigitaliseraBetyg igen efter genomförda rättelser."]
-        
-        self.metadata_category = DigitaliseraBetygSettings().get_metadata_category()
-
-    # Felrättningsfönstret för applikationen.
+        sg.theme(Data.theme())
+    
     def gui_error(self, error_list):
-            sg.theme(self.theme)
-
-            window = sg.Window(
-                title="Felinformation", 
-                layout=[  
-                    [sg.Text(f"Följande fel upptäcktes som behöver rättas innan programmet öppnas igen:")],
-                    [sg.Output(expand_x=True, size=(None, 10), font=self.font_output)],
-                    ], 
-                font=(self.font), 
-                icon=self.icon, 
-                finalize=True)
-            
-            # Skriver ut vad som är felaktigt i inställningsfilen.
-            for row in error_list + self.error_list_end:
-                print(row)
-
+            """Felrättningsfönstret för programmet."""
+            layout = [[sg.Text("Följande fel upptäcktes som behöver rättas innan programmet öppnas igen:")]]
+            for row in error_list:
+                 layout.append([sg.Text(row)])
+            window = sg.Window(title="Felinformation", layout=layout, font=Data.font("grade"), icon=Data.path_icon())
             while True:
                 event, values = window.read()
                 match event:
-                    case sg.WIN_CLOSED:
-                        break
+                    case sg.WIN_CLOSED: break
             window.close()
             sys.exit()
 
-    # Huvudfönstret för applikationen.
-    def gui_main(self, max_students, schools, gradetypes, scan_path, institution):
-            sg.theme(self.theme)
-            
-            category = [[
-                sg.Text("ID", size=(3)), 
-                sg.Text(self.metadata_category[0], size=(20)), 
-                sg.Text(self.metadata_category[1], size=(15)), 
-                sg.Text(self.metadata_category[2], size=(20)),
-                sg.Text(self.metadata_category[3], size=(6)),
-                sg.Text(self.metadata_category[4], size=(6)),
-                sg.Text("Betygstyp", size=(13)), # "Handlingstyp" i metadatafilen, "betygstyp" i GUI för att vara enkel att förstå.
-                ]]
-            
+    def gui_main(self):
+            """Huvudfönstret för programmet"""
+            metadata = Data.metadata_grade()
+            metadata_size = Data.metadata_grade_size()
+            data = Data()
+            schools = data.schools()
+            gradetypes = data.gradetypes()
+            forms = data.forms()
+            max_students = data.max_students()
+            folder_scan = data.folder_scan()
+            institution = data.institution()
+            del data
+
+            category = [[sg.Text(data, size=size) for data, size in zip(metadata, metadata_size)]]
             fields= [[
                 sg.Text(student+1, size=(3)), 
-                sg.Input(key=f"{student}{self.metadata_category[0]}", visible=False, size=(20)), 
-                sg.Input(key=f"{student}{self.metadata_category[1]}", visible=False, size=(15)), 
-                sg.Combo(schools, key=f"{student}{self.metadata_category[2]}", visible=False, size=(19)),
-                sg.Input(key=f"{student}{self.metadata_category[3]}", visible=False, size=(6)), 
-                sg.Input(key=f"{student}{self.metadata_category[4]}", visible=False, size=(6)), 
-                sg.Combo(gradetypes, key=f"{student}{self.metadata_category[5]}", visible=False, size=(11)), 
+                sg.Input(key=f"{student}{metadata[1]}", visible=False, size=metadata_size[1]), 
+                sg.Input(key=f"{student}{metadata[2]}", visible=False, size=metadata_size[2]), 
+                sg.Input(key=f"{student}{metadata[3]}", visible=False, size=metadata_size[3]),
+                sg.Input(key=f"{student}{metadata[4]}", visible=False, size=metadata_size[4]), 
+                sg.Combo(gradetypes, key=f"{student}{metadata[5]}", visible=False, size=metadata_size[5]-1), 
+                sg.Combo(schools, key=f"{student}{metadata[6]}", visible=False, size=metadata_size[6]-1), 
+                sg.Combo(forms, key=f"{student}{metadata[7]}", visible=False, size=metadata_size[7]-1), 
                 ] for student in range(max_students)]
-
-            layout = [[
-                    sg.FileBrowse("Hämta betygsfil", file_types=(("ALL Files", ".pdf"),), initial_folder=scan_path, target="file_path", key="file_path", enable_events=True), 
-                    sg.Text(key="file_path_text", size=(50)), 
-                    sg.Push(), 
-                    sg.Button("Visa betygsfil", key="open_file", enable_events=True), 
-                    sg.Button("Hjälp", enable_events=True),
-                    ],
+            layout_grade = [
+                [sg.FileBrowse("Hämta betygsfil", file_types=(("", ".pdf"),), initial_folder=folder_scan, 
+                                target="file_path", enable_events=True), 
+                    sg.Input(key="file_path", font=Data.font("output"), expand_x=True, enable_events=True, readonly=True, 
+                             disabled_readonly_background_color=sg.theme_background_color()), 
+                    sg.Button("Visa betygsfil", key="open_file", enable_events=True)],
                 [sg.Column(category)],
                 [sg.Column(fields)],
                 [sg.Button("Digitalisera betyg"), sg.Push(), sg.Button("Avsluta")],
-                [sg.Output(key="output", size=(None, 8), expand_x=True, font=self.font_output)],
-                ]    
+                [sg.Output(key="output", size=(None, 5), expand_x=True, font=Data.font("output"))],
+                ]  
+              
+            layout_help = [
+                [sg.Text("1. Skanna så många betyg som ni känner att ni vill hantera samtidigt. " \
+                 f"Dock inte så att sidantalet i den skannade filen överstiger {max_students} sidor.")], 
+                [sg.Text("2. Tänk på att betygen måste vara en sida vardera, " \
+                "programmet kan inte hantera flersidiga individuella betyg.")],
+                [sg.Text("3. Hämta pdf-filen med inskannade betyg via 'Hämta betygsfil'.")],
+                [sg.Text("4. Programmet kommer försöka hämta information från betygen och placera dessa i rätt fält.")], 
+                [sg.Text("5. Den hämtade pdf-filen kan visas i webbläsaren via 'Visa betygsfilen'. " \
+                "ID:t för varje elev är samma som sidan i pdf-filen.")],
+                [sg.Text("6. Fyll i resterande fält med rätt uppgifter, " \
+                "samt säkerställ att de uppgifter som programmet hämtade är korrekta.")], 
+                [sg.Text("7. När samtliga fält är ifyllda, välj 'Digitalisera betyg'.")],
+                ]
             
-            window = sg.Window(
-                title=f"{institution} - Digitalisera betyg", 
-                layout=layout, 
-                font=(self.font), 
-                icon=self.icon, 
-                finalize=True
-                )
+            layout = [[sg.TabGroup([[sg.Tab("Digitalisera betyg", layout_grade), sg.Tab("Hjälp", layout_help)]])]]  
+            window = sg.Window(title=f"{institution} - Digitalisera betyg", layout=layout, font=Data.font("grade"), 
+                               icon=Data.path_icon(), finalize=True)
             
-            # Löser en bugg där text efter val i combomenyn är dold bakom heltäckande färg. Outlinen av markeringen är dock kvar, menmen.....
+            # Löser en bugg där text efter val i combomenyn är dold bakom heltäckande färg. 
+            # Outlinen av markeringen är dock kvar.
             # Lösningen är hämtad från https://stackoverflow.com/a/73225157.
             style = sg.ttk.Style()
             for student in range(max_students):
-                style.configure(window[f"{student}{self.metadata_category[2]}"].widget.configure()['style'][-1], selectforeground=sg.theme_input_text_color())
-                style.configure(window[f"{student}{self.metadata_category[5]}"].widget.configure()['style'][-1], selectforeground=sg.theme_input_text_color())
-            
-            return self.error_color, window
-    
-    # Kort tillvägagångsinformation som skrivs ut i huvudfönstret.
-    def help_text(self, max_students):
-         return [
-            f"1. Skanna så många betyg som ni känner att ni vill hantera samtidigt. Dock inte så att sidantalet i den skannade filen överstiger {max_students} sidor.", 
-            "2. Tänk på att betygen måste vara enkelsidiga, programmet kan inte hantera flersidiga individuella betyg.",
-            "3. Hämta pdf-filen med inskannade betyg via 'Välj betygsfil'.",
-            "4. Programmet kommer försöka hämta information från betygen och placera dessa i rätt fält.", 
-            "5. Den hämtade pdf-filen kan visas i webbläsaren via 'Visa betygsfilen'. ID:t för varje elev är samma som sidan i pdf-filen.",
-            "6. Fyll i resterande fält med rätt uppgifter, samt säkerställ att de uppgifter som programmet hämtade är korrekta.", 
-            "7. När samtliga fält är ifyllda, välj 'Digitalisera betyg'.",
-            ]
+                for data in metadata[5:]:
+                    style.configure(window[f"{student}{data}"].widget.configure()['style'][-1], 
+                                    selectforeground=sg.theme_input_text_color())
+
+            return window
+
+
+if __name__ == "__main__":
+     pass
